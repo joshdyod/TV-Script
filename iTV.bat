b6e8c03cd91223dac4de9acebc4f577613056d99
Josh Yoder <joshdyod@live.com>
2019-01-07 12:06:00 -0500

    Update iTV.bat

--- iTV.bat
+++ iTV.bat
@@ -1,3 +1,3 @@
 @echo off
 :start
-"C:\Program Files\WindowsApps\VideoLAN.VLC_3.0.3.0_x64__paz6r1rewnh0a\VLC.Universal.exe" --playlist-autostart --fullscreen --loop --playlist-tree C:\Saturday Monrinig Dopeness\
+"C:\Program Files\WindowsApps\VideoLAN.VLC_3.0.3.0_x64__paz6r1rewnh0a\VLC.Universal.exe" --playlist-autostart --fullscreen --loop --playlist-tree C:\Saturday TV\
